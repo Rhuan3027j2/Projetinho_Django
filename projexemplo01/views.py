0473cbcb8638ced45b1abec2a31f9d60539b0b98
RHUAN RAFAEL MILARES ALVARINHO <025732@fatecararas.com>
2026-04-09 11:04:48 -0300

    firts commit

---
+++ projexemplo01/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Papo de udaia")
+
+def Sobre(request):
+    return HttpResponse("FINESSEEEEE")
+
+def Autor(request):
+    return HttpResponse("DEADHARDDD")
+
+def Notias(request):
+    return HttpResponse("Gen rush")
+
+def Empresa(request):
+    return HttpResponse("Tunelador INfernalllllll!!!!!!!!!!!!!")
